899c671d77031b69442f8f6916cf3337b4835508
WendolynQuartey <quarteywendolyn@gmail.com>
2025-12-11 19:07:14 -0500

    Part 3 works

--- script.js
+++ script.js
@@ -2,20 +2,18 @@ let csvData = `ID,Name,Occupation,Age\n42,Bruce,Knight,41\n57,Bob,Fry Cook,19\n6
 
 let cell = "";
 let row = [];
+let saveRow;
 let csvArray = [];
+let columns = 0;
 
 for (let i = 0; i < csvData.length; i++) {
-   console.log("csvData[i]: ", csvData[i]);
-   console.log("i: ", i);
-   console.log("Cell: ", cell);
-   if (csvData[i] === ','){ // check if the value of  csvData at index i is equal to the chracter ','
+    if (csvData[i] === ',') { // check if the value of  csvData at index i is equal to the chracter ','
       row.push(cell);
-      console.log("Row: ", row);
       cell = "";
-   } 
-   else if (csvData[i] === '\n' || i == csvData.length-1){
-      if(i == csvData.length-1){
-          cell += csvData[i];
+   }
+   else if (csvData[i] === '\n' || i == csvData.length - 1) {
+      if (i == csvData.length - 1) {
+         cell += csvData[i];
       }
       row.push(cell);
       csvArray.push(row);
@@ -24,9 +22,36 @@ for (let i = 0; i < csvData.length; i++) {
    } else {
       cell += csvData[i];   // add the value of csvData at index i
    }
+
 }
 
 console.log(csvArray);
-console.log(csvData.length-1);
+
+
+// Part 3
+
+// // For each row of data in the result array produced by your code above, 
+// create an object where the key of each value is the heading for that value’s column.
+// // Convert these keys to all lowercase letters for consistency.
+// // Store these objects in an array, in the order that they were originally listed.
+// // Since the heading for each column will be stored in the object keys, 
+// you do not need to create an object for the heading row itself.
+
+
+let newArray = [];
+let csvKeys = [];
+
+for (let i = 1; i < csvArray.length; i++) {
+   let csvObject = {};
+   for (let x = 0; x < csvArray[i].length; x++) {
+      csvKeys.push(String(csvArray[0][x]).toLowerCase());
+      csvObject[csvKeys[x]] = csvArray[i][x];
+   }
+   newArray.push(csvObject);
+}
+
+console.log(newArray);
+
+
 
 
